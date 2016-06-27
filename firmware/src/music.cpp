5f29b0e782801152f6253ba457169aa495466687
Rlineatte <lineatterobin@gmail.com>
2016-06-27 17:12:50 +0200

    Modification des mouvelents musicaux et ajout d'un réglage fréquence global

--- firmware/src/music.cpp
+++ firmware/src/music.cpp
@@ -20,6 +20,7 @@
 #include <rhock/stream.h>
 #endif
 
+#include "function.h"
 #include "kinematic.h"
 #include "music.h"
 
@@ -32,14 +33,14 @@ TERMINAL_PARAMETER_FLOAT(freqLeg4, "Leg 4's freq", 0.0);
 #define MODE_GLIDE      1
 #define MODE_SCRATCH    2
 
-TERMINAL_PARAMETER_INT(modeLeg1, "Mode (0:Perc, 1:TODO)", MODE_PERC);
-TERMINAL_PARAMETER_INT(modeLeg2, "Mode (0:Perc, 1:TODO)", MODE_PERC);
-TERMINAL_PARAMETER_INT(modeLeg3, "Mode (0:Perc, 1:TODO)", MODE_PERC);
-TERMINAL_PARAMETER_INT(modeLeg4, "Mode (0:Perc, 1:TODO)", MODE_PERC);
+TERMINAL_PARAMETER_INT(modeLeg1, "Mode (0:Perc, 1:GLIDE, 2:SCRATCH)", MODE_PERC);
+TERMINAL_PARAMETER_INT(modeLeg2, "Mode (0:Perc, 1:GLIDE, 2:SCRATCH)", MODE_PERC);
+TERMINAL_PARAMETER_INT(modeLeg3, "Mode (0:Perc, 1:GLIDE, 2:SCRATCH)", MODE_PERC);
+TERMINAL_PARAMETER_INT(modeLeg4, "Mode (0:Perc, 1:GLIDE, 2:SCRATCH)", MODE_PERC);
 
-TERMINAL_PARAMETER_FLOAT(phaseLeg1, "Leg 1's phase", 0.5);
+TERMINAL_PARAMETER_FLOAT(phaseLeg1, "Leg 1's phase", 0.0);
 TERMINAL_PARAMETER_FLOAT(phaseLeg2, "Leg 2's phase", 0.0);
-TERMINAL_PARAMETER_FLOAT(phaseLeg3, "Leg 3's phase", 0.5);
+TERMINAL_PARAMETER_FLOAT(phaseLeg3, "Leg 3's phase", 0.0);
 TERMINAL_PARAMETER_FLOAT(phaseLeg4, "Leg 4's phase", 0.0);
 
 
@@ -58,54 +59,86 @@ TERMINAL_COMMAND(freqLegs, "Set all freqLeg") {
     }
 }
 
+Function perc;
+Function glide;
+
+void setup_music_functions()
+{
+    perc.clear();
+    glide.clear();
+
+    perc.addPoint(0.0, 0.0);
+    perc.addPoint(0.1, 30.0);
+    perc.addPoint(0.3, 30.0);
+    perc.addPoint(0.35, 0.0);
+    perc.addPoint(1.0, 0.0);
+
+    glide.addPoint(0.0, 0.0);
+    glide.addPoint(0.2, -25.0);
+    glide.addPoint(0.5, -50.0);
+    glide.addPoint(0.8, -25.0);
+    glide.addPoint(1.0, 0.0);
+
+}
+
 #define TMAX 10000
 float tps = 0;
 
-float motion_music(int motor, int leg) {
+float motion_music(int motor, int leg, float freq) {
 
-    float freq;
+    setup_music_functions();
+
+    float freqLeg;
     float phase;
     int mode;
 
-    tps++;
-    if (tps > TMAX) {
-        tps -= TMAX;
+    if (motor==3) {
+        tps += freq;
+        if (tps > TMAX) {
+            tps -= TMAX;
+        }
+        if (tps < 0.0) tps+=freq;
     }
-    if (tps < 0.0) tps++;
 
     switch(leg) {
     case 0:
         phase = phaseLeg1;
-        freq = freqLeg1;
+        freqLeg = freqLeg1;
         mode = modeLeg1;
         break;
     case 1:
         phase = phaseLeg2;
-        freq = freqLeg2;
+        freqLeg = freqLeg2;
         mode = modeLeg2;
         break;
     case 2:
         phase = phaseLeg3;
-        freq = freqLeg3;
+        freqLeg = freqLeg3;
         mode = modeLeg3;
         break;
     case 3:
         phase = phaseLeg4;
-        freq = freqLeg4;
+        freqLeg = freqLeg4;
         mode = modeLeg4;
         break;
     default:
-        freq = 0;
+        freqLeg = 0;
         mode = MODE_PERC;
         break;
     }
 
     switch(mode) { //Add more modes in this switch
     case MODE_PERC:
-        return calc_angle_perc(motor, freq, phase);
+        if (motor==2)
+            return perc.getMod((freqLeg*tps)/(TMAX*1.0) + phase);
+        else
+            return 0.0;
         break;
     case MODE_GLIDE:
-        return calc_angle_glide(motor, freq, phase);
+        if (motor==3 || motor==2)
+            return glide.getMod((freqLeg*tps)/(TMAX*1.0) + phase);
+        else
+            return 0.0;
         break;
     case MODE_SCRATCH:
         return calc_angle_scratch(motor, freq, phase);
@@ -118,49 +151,50 @@ float motion_music(int motor, int leg) {
 
 }
 
+//OBSOLETE
 //Mode Perc calculation
-float calc_angle_perc(int motor, float freq, float phase) {
-    float time;
-    switch(motor) {
-    case 1:
-        return 0.0;
-        break;
-    case 2:
-        time = fmod((freq*tps)/(TMAX*1.0),1)*3.14;
-        time += phase*3.14;
-        return (abs(sin(time))-abs(cos(time-3.14/2.5))+0.3)*30.0;
-        break;
-    case 3:
-        return 0.0;
-        break;
-    default:
-        return 0.0;
-        break;
-    }
-}
+//float calc_angle_perc(int motor, float freq, float phase) {
+//    float time;
+//    switch(motor) {
+//    case 1:
+//        return 0.0;
+//        break;
+//    case 2:
+//        time = fmod((freq*tps)/(TMAX*1.0),1)*3.14;
+//        time += phase*3.14;
+//        return (abs(sin(time))-abs(cos(time-3.14/2.5))+0.3)*30.0;
+//        break;
+//    case 3:
+//        return 0.0;
+//        break;
+//    default:
+//        return 0.0;
+//        break;
+//    }
+//}
 
 //Mode Glide calculation
-float calc_angle_glide(int motor, float freq, float phase) {
-    float time;
-    switch(motor) {
-    case 1:
-        return 0.0;
-        break;
-    case 2:
-        time = fmod((freq*tps)/(TMAX*1.0),1)*3.14;
-        time += phase*3.14;
-        return ((abs(cos(time)))-1)*50.0;
-        break;
-    case 3:
-        time = fmod((freq*tps)/(TMAX*1.0),1)*3.14;
-        time += phase*3.14;
-        return ((abs(cos(time)))-1)*50.0;
-        break;
-    default:
-        return 0.0;
-        break;
-    }
-}
+//float calc_angle_glide(int motor, float freq, float phase) {
+//    float time;
+//    switch(motor) {
+//    case 1:
+//        return 0.0;
+//        break;
+//    case 2:
+//        time = fmod((freq*tps)/(TMAX*1.0),1)*3.14;
+//        time += phase*3.14;
+//        return ((abs(cos(time)))-1)*50.0;
+//        break;
+//    case 3:
+//        time = fmod((freq*tps)/(TMAX*1.0),1)*3.14;
+//        time += phase*3.14;
+//        return ((abs(cos(time)))-1)*50.0;
+//        break;
+//    default:
+//        return 0.0;
+//        break;
+//    }
+//}
 
 //Mode Scratch calculation
 float calc_angle_scratch(int motor, float freq, float phase) {
--- firmware/src/music.h
+++ firmware/src/music.h
@@ -1,10 +1,12 @@
 #ifndef _METABOT_MUSIC_H
 #define _METABOT_MUSIC_H
 
-float motion_music(int motor, int leg);
-float calc_angle_perc(int motor, float freq, float phase);
-float calc_angle_glide(int motor, float freq, float phase);
-float calc_angle_scratch(int motor, float freq, float phase);
+float motion_music(int motor, int leg, float freq);
+
+//OBSOLETE
+//float calc_angle_perc(int motor, float freqLeg, float phase);
+//float calc_angle_glide(int motor, float freqLeg, float phase);
+float calc_angle_scratch(int motor, float freqLeg, float phase);
 
 #endif
 
